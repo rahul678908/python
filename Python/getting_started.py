c62282d9f3328fdfc9b3ef9cbbe47a5c82e9821c
rahul678908 <rahulkannan33886@gmail.com>
2024-09-06 16:08:36 +0530

    Python getting started

---
+++ Python/getting_started.py
@@ -0,0 +1,50 @@
+addition = 1 + 2
+print (addition)
+
+powers = 2 ** 1000
+print(powers)
+
+floatingpointnumbers = 1.2 + 2.3
+print(floatingpointnumbers)
+
+x = "hello world"
+print(x)
+
+quotes = 'hello' + 'world'
+print(quotes)
+doublequotes =  "hello" *  3
+print(doublequotes)
+print("=" * 40)
+
+length = len('helloworld')
+print(length)
+
+text = """This is a multi line sting.
+Line2
+Line3
+and the text may have "quotes" too
+"""
+print(text)
+
+print("a\nb\nc")
+
+x = ["a", "b", "c"]
+print(x)
+lenfdfd = len(x)
+print(len)
+letter = x[2]
+print(letter)
+
+x = 5
+print(x)
+x = 'hello'
+print(x)
+
+print('hello')
+print('hello', 1, 2, 3)
+
+v = len("hello")
+print(v)
+
+bqr = len(['a','b','c'])
+print(bqr)
---
+++ Python/working_with_data.py
@@ -0,0 +1,41 @@
+u = range(1, 4)
+print(u)
+u[0]
+print(u[0])
+a = u[0] + u[1]
+print(a)
+
+q = [1,2,3,4]
+e = len(q)
+print(e)
+
+j = [1,2,3]
+k = [4,5]
+print(j+k) 
+l = 3
+print(k*3)
+
+n = [1,2]
+print(n[0])
+print(n[1])
+
+"<stdin>"
+b = [1, 2, 3, 4]
+print(b[-1])
+print(b[-2])
+
+"<stdin>"
+x = [1,2,3,4]
+print(x[0:2])
+print(x[1:4])
+print(x[0:-1])
+
+p = [1,2,3,4]
+print(p[:2])
+print(p[2:])
+print(p[:])
+
+z = range(10)
+print(z)
+print(z[0:6:2])
+print(z[::-1])
